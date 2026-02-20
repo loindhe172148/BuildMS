7dda6c6b7c932cf6d071758ee410de8c7788b6aa
Uoche172248 <146301598+Nguyenquangbauoc@users.noreply.github.com>
2026-02-20 15:52:06 +0700

    update

--- src/main/java/controller/InventoryController.java
+++ src/main/java/controller/InventoryController.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import Service.InventoryService;
+import service.InventoryService;
 
 import java.io.IOException;
 import java.util.List;
